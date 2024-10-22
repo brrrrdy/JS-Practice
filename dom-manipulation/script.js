9e9e66454cdf317ed43e32f572380ae97d34a1d9
Tom Alvarez <127635396+brrrrdy@users.noreply.github.com>
2024-10-22 12:35:03 +0200

    h3 added

--- dom-manipulation/script.js
+++ dom-manipulation/script.js
@@ -2,3 +2,8 @@ const pElement = document.createElement('p');
 pElement.textContent = "Hey I'm red!";
 pElement.style.color = 'red';
 document.body.appendChild(pElement);
+
+const hElement = document.createElement('h3');
+hElement.textContent = "Hey I'm a blue h3!";
+hElement.style.color = 'blue';
+document.body.appendChild(hElement);
