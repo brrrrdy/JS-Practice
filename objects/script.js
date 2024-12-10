3fe82684d082439d62aa4fbec8a53197a2e9a07c
Tom Alvarez <127635396+brrrrdy@users.noreply.github.com>
2024-12-10 16:45:07 +0100

    challenge 2

--- objects/script.js
+++ objects/script.js
@@ -1,9 +1,14 @@
-let user = {
-    name: 'John',
-    surname: 'Smith'
-};
+function isEmpty(obj){
+    for (let key in obj) {
+        return false;
+    }
+    return true;
+}
 
-console.log(user);
+let schedule = {};
 
-user.name = 'Pete';
-delete user.name;
+console.log(isEmpty(schedule)); // true
+
+schedule["8:30"] = "get up";
+
+console.log(isEmpty(schedule)); // false
