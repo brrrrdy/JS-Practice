0b08fab161602d4860bc2400ba07c0b5c4e04655
Tom Alvarez <127635396+brrrrdy@users.noreply.github.com>
2025-01-20 16:40:04 +0100

    js practice

--- objects/script.js
+++ objects/script.js
@@ -1,15 +1,32 @@
-let salaries = {
-    John: 100,
-    Ann: 160,
-    Pete: 130
-};
-
-function sumSalaries(salaries) {
+function sumOfTripledEvens(array) {
     let sum = 0;
-    for (let salary in salaries) {
-        sum += salaries[salary];
+    for (let i = 0; i < array.length; i++) {
+      // Step 1: If the element is an even number
+      if (array[i] % 2 === 0) {
+        // Step 2: Multiply this number by three
+        const tripleEvenNumber = array[i] * 3;
+  
+        // Step 3: Add the new number to the total
+        sum += tripleEvenNumber;
+      }
     }
     return sum;
-}
+  }
 
-console.log(sumSalaries(salaries)); // 390
+  /*
+
+  As an example of what we mean, let’s consider a sumOfTripledEvens function. It will:
+
+    Take in an array.
+    For every even number, it will triple it.
+    Then it will sum all those even numbers.
+
+Can you try thinking of how you could implement a function like that using pseudocode?
+
+    We need to perform an operation only on the even numbers.
+    We need to transform those numbers by multiplying them by 3.
+    Finally, we need to add the result up from the previous transformation.
+
+So using that logic, you may end up implementing something like this:
+
+*/
