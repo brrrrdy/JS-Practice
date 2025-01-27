8657d4586f0c53fce9d1987d816e77374f08efde
Tom Alvarez <127635396+brrrrdy@users.noreply.github.com>
2025-01-27 13:15:12 +0100

    camelize completed

--- objects/camelize.js
+++ objects/camelize.js
@@ -1,9 +1,10 @@
 function camelize (str) {
     /* const str = "list-style-image"; */
-    return str.split('-')
+    return str.split('-').map((word, index) => index === 0 ? word : word.charAt(0).toUpperCase() + word.slice(1)).join('');
 }
 
 const str = "list-style-image";
+console.log(camelize(str));
 
 /* 
 
