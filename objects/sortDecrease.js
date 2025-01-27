e3cc1b2e1f596ec65e776596a805748701224dfd
Tom Alvarez <127635396+brrrrdy@users.noreply.github.com>
2025-01-27 15:48:56 +0100

    sortDecrease completed

---
+++ objects/sortDecrease.js
@@ -0,0 +1,12 @@
+function sortDecrease (arr) {
+return arr.sort((a, b) => a - b);
+}
+
+const arr = [5, 2, 1, -10, 8];
+
+console.log(sortDecrease(arr));
+/*
+
+sort an array in increasing order and return the array
+
+*/
