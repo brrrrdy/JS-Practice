6846067e0ee558049400be9a8c20c97e9298d629
Tom Alvarez <127635396+brrrrdy@users.noreply.github.com>
2025-01-27 12:55:46 +0100

    camelize

---
+++ objects/camelize.js
@@ -0,0 +1,14 @@
+function camelize (str) {
+    /* const str = "list-style-image"; */
+    return str.split('-')
+}
+
+const str = "list-style-image";
+
+/* 
+
+1. split string into an array
+2. transform the array
+3. join the array back
+
+*/
