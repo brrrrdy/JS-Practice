de1a27bf83d0921e81a600207f70e7883f7c716a
Tom Alvarez <127635396+brrrrdy@users.noreply.github.com>
2025-01-29 15:32:31 +0100

    groupByIds

---
+++ objects/groupById.js
@@ -0,0 +1,17 @@
+function groupById(users) {
+
+}
+
+let users = [
+    {id: 'john', name: "John Smith", age: 20},
+    {id: 'ann', name: "Ann Smith", age: 24},
+    {id: 'pete', name: "Pete Peterson", age: 31},
+  ];
+  
+/*
+
+Let’s say we received an array of users in the form {id:..., name:..., age:... }.
+
+Create a function groupById(arr) that creates an object from it, with id as the key, and array items as values.
+
+*/
