a4989d79d1ecd3a457a0c5435694f4a8e703a533
Tom Alvarez <127635396+brrrrdy@users.noreply.github.com>
2025-04-14 16:03:51 +0200

    factory functions

--- sandbox/script.js
+++ sandbox/script.js
@@ -1,16 +1,83 @@
-function Book(title, author, pages, read) {
-  this.title = title;
-  this.author = author;
-  this.pages = pages;
-  this.read = read;
+// let globalAge = 23; // This is a global variable
 
-  this.info = function () {
-    const readStatus = this.read ? "read" : "not read";
-    return `${this.title} by ${this.author}, ${this.pages} pages, ${readStatus}`;
-  };
+// // This is a function - and hey, a curly brace indicating a block
+// function printAge (age) {
+//   var varAge = 34; // This is a function scoped variable
+
+//   // This is yet another curly brace, and thus a block
+//   if (age > 0) {
+//     // This is a block-scoped variable that exists
+//     // within its nearest enclosing block, the if's block
+//     const constAge = age * 2;
+//     console.log(constAge);
+//   }
+
+//   // ERROR! We tried to access a block scoped variable
+//   // not within its scope
+//   console.log(constAge);
+// }
+
+// printAge(globalAge);
+
+// // ERROR! We tried to access a function scoped variable
+// // outside the function it's defined in
+// console.log(varAge);
+
+// function makeAdding(firstNumber) {
+//   // "first" is scoped within the makeAdding function
+//   const first = firstNumber;
+//   return function resulting(secondNumber) {
+//     // "second" is scoped within the resulting function
+//     const second = secondNumber;
+//     return first + second;
+//   };
+// }
+// // but we've not seen an example of a "function"
+// // being returned, thus far - how do we use it?
+
+// const add5 = makeAdding(5);
+// console.log(add5(2)); // logs 7
+
+// const User = function (name) {
+//   this.name = name;
+//   this.discordName = "@" + name;
+// };
+// // hey, this is a constructor -
+// // then this can be refactored into a factory!
+
+// function createUser(name) {
+//   const discordName = "@" + name;
+//   return { name, discordName };
+// }
+// // and that's very similar, except since it's just a function,
+// // we don't need a new keyword
+
+// console.log(createUser("Tom"));
+
+// function createUser(name) {
+//   const discordName = "@" + name;
+
+//   let reputation = 0;
+//   const getReputation = () => reputation;
+//   const giveReputation = () => reputation++;
+
+//   return { name, discordName, getReputation, giveReputation };
+// }
+
+// const josh = createUser("josh");
+// josh.giveReputation();
+// josh.giveReputation();
+
+// console.log({
+//   discordName: josh.discordName,
+//   reputation: josh.getReputation(),
+// });
+// // logs { discordName: "@josh", reputation: 2 }
+
+const first = "wes";
+let second = "bos";
+var age = 100;
+
+function sayHi() {
+  console.log("hi!");
 }
-
-const theHobbit = new Book("The Hobbit", "J.R.R. Tolkien", 295, false);
-
-console.log(Object.getPrototypeOf(Book.prototype) === Object.prototype);
-console.log(theHobbit.valueOf());
