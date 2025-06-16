81fd2dc3d27e657e33efd5eae69b5f3d215d235a
Tom Alvarez <127635396+brrrrdy@users.noreply.github.com>
2025-06-16 16:16:45 +0200

    init

---
+++ oop/src/script.js
@@ -0,0 +1,20 @@
+import logMessage from "./logger.js";
+
+class CalorieTracker {
+  constructor(maxCalories) {
+    this.maxCalories = maxCalories;
+    this.currentCalories = 0;
+  }
+
+  trackCalories(calorieCount) {
+    this.currentCalories += calorieCount;
+    if (this.currentCalories > this.maxCalories) {
+      logMessage("You've gone over your max calories for today!");
+    }
+  }
+}
+
+const tracker = new CalorieTracker(2500);
+tracker.trackCalories(500);
+tracker.trackCalories(1000);
+tracker.trackCalories(700);
