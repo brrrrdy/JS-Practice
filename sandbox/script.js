2b992337bd1e0d1728eed0e9cd47fad6afa88175
Tom Alvarez <127635396+brrrrdy@users.noreply.github.com>
2025-08-04 16:24:40 +0200

    codewars

--- sandbox/script.js
+++ sandbox/script.js
@@ -1,14 +1,11 @@
-let p = new Promise((resolve, reject) => {
-  let a = 1 + 1;
-  if (a == 2) {
-    resolve("success");
-  } else {
-    reject("failed");
-  }
-});
+var uniqueInOrder = function (iterable) {
+  //your code here - remember iterable can be a string or an array
+};
 
-p.then((message) => {
-  console.log("this is in the theesd " + message);
-}).catch((message) => {
-  console.log("this is in the catch " + message);
-});
+// Implement the function unique_in_order which takes as argument a sequence and returns a list of items without any elements with the same value next to each other and preserving the original order of elements.
+
+// For example:
+
+// uniqueInOrder('AAAABBBCCDAABBB') == ['A', 'B', 'C', 'D', 'A', 'B']
+// uniqueInOrder('ABBCcAD')         == ['A', 'B', 'C', 'c', 'A', 'D']
+// uniqueInOrder([1,2,2,3,3])       == [1,2,3]
